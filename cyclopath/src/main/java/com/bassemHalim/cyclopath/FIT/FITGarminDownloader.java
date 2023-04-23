51fc061e5ce5cccc5d2d40464aadf8a988d26069
BassemHalim <bassemamir459@gmail.com>
2023-04-23 15:12:33 -0700

    login implemented with playwright

--- cyclopath/src/main/java/com/bassemHalim/cyclopath/FIT/FITGarminDownloader.java
+++ cyclopath/src/main/java/com/bassemHalim/cyclopath/FIT/FITGarminDownloader.java
@@ -3,6 +3,10 @@ package com.bassemHalim.cyclopath.FIT;
 import com.microsoft.playwright.*;
 import com.microsoft.playwright.options.RequestOptions;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
@@ -20,10 +24,11 @@ public class FITGarminDownloader implements FITDownloader {
     private String garmin_connect_base_url = "https://connect.garmin.com";
     private String garmin_connect_enus_url = garmin_connect_base_url + "/en-US";
     private String garmin_connect_sso_base_url = "https://sso.garmin.com/sso/";
-    private String garmin_connect_sso_login_url = garmin_connect_sso_base_url + "signin";
+    private String garmin_connect_sso_signin_url = garmin_connect_sso_base_url + "signin";
+    private String garmin_sso_params_url = "https://sso.garmin.com/sso/signin?service=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&webhost=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&source=https%3A%2F%2Fconnect.garmin.com%2Fsignin%2F&redirectAfterAccountLoginUrl=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&redirectAfterAccountCreationUrl=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&gauthHost=https%3A%2F%2Fsso.garmin.com%2Fsso&locale=en_US&id=gauth-widget&cssUrl=https%3A%2F%2Fconnect.garmin.com%2Fgauth-custom-v1.2-min.css&privacyStatementUrl=https%3A%2F%2Fwww.garmin.com%2Fen-US%2Fprivacy%2Fconnect%2F&clientId=GarminConnect&rememberMeShown=true&rememberMeChecked=false&createAccountShown=true&openCreateAccount=false&displayNameShown=false&consumeServiceTicket=false&initialFocus=true&embedWidget=false&socialEnabled=false&generateExtraServiceTicket=true&generateTwoExtraServiceTickets=true&generateNoServiceTicket=false&globalOptInShown=true&globalOptInChecked=false&mobile=false&connectLegalTerms=true&showTermsOfUse=false&showPrivacyPolicy=false&showConnectLegalAge=false&locationPromptShown=true&showPassword=true&useCustomHeader=false&mfaRequired=false&performMFACheck=false&rememberMyBrowserShown=true&rememberMyBrowserChecked=false";
     private String garmin_connect_login_url = garmin_connect_enus_url + "/signin";
     private String garmin_connect_css_url = "https://static.garmincdn.com/com.garmin.connect/ui/css/gauth-custom-v1.2-min.css";
-    private String garmin_connect_privacy_url = "//connect.garmin.com/en-U/privacy";
+    private String garmin_connect_privacy_url = "//connect.garmin.com/en-US/privacy";
     private String garmin_connect_user_profile_url = "proxy/userprofile-service/userprofile";
     private String garmin_connect_activity_search_url = "proxy/activitylist-service/activities/search/activities";
     private String garmin_connect_usersummary_url = "proxy/usersummary-service/usersummary";
@@ -34,51 +39,27 @@ public class FITGarminDownloader implements FITDownloader {
     public FITGarminDownloader(String username, String password) {
         Username = username;
         Password = password;
-        try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch();
-            Page page = browser.newPage();
-            APIResponse response = page.request().get(this.garmin_connect_sso_login_url, RequestOptions.create()
-                    .setQueryParam("service", this.garmin_connect_login_url + "/modern/")
-                    .setQueryParam("webhost", this.garmin_connect_base_url)
-                    .setQueryParam("source", this.garmin_connect_login_url)
-                    .setQueryParam("redirectAfterAccountLoginUrl", this.garmin_connect_login_url + "/modern/")
-                    .setQueryParam("redirectAfterAccountCreationUrl", this.garmin_connect_login_url + "/modern/")
-                    .setQueryParam("gauthHost", this.garmin_connect_sso_base_url)
-                    .setQueryParam("locale", "en_US")
-                    .setQueryParam("id", "gauth-widget")
-                    .setQueryParam("cssUrl", this.garmin_connect_css_url)
-                    .setQueryParam("privacyStatementUrl", this.garmin_connect_privacy_url)
-                    .setQueryParam("clientId", "GarminConnect")
-                    .setQueryParam("rememberMeShown", "true")
-                    .setQueryParam("rememberMeChecked", "false")
-                    .setQueryParam("createAccountShown", "true")
-                    .setQueryParam("openCreateAccount", "false")
-                    .setQueryParam("displayNameShown", "false")
-                    .setQueryParam("consumeServiceTicket", "false")
-                    .setQueryParam("initialFocus", "true")
-                    .setQueryParam("embedWidget", "false")
-                    .setQueryParam("generateExtraServiceTicket", "true")
-                    .setQueryParam("generateTwoExtraServiceTickets", "false")
-                    .setQueryParam("generateNoServiceTicket", "false")
-                    .setQueryParam("globalOptInShown", "true")
-                    .setQueryParam("globalOptInChecked", "false")
-                    .setQueryParam("mobile", "false")
-                    .setQueryParam("connectLegalTerms", "true")
-                    .setQueryParam("locationPromptShown", "true")
-                    .setQueryParam("showPassword", "true"));
-            if (response.ok()) {
-//                System.out.println("title: " + response.text());
-                // match " name="_csrf" value="" " from the html response
-                Pattern pattern = Pattern.compile("name=\"_csrf\" value=\"");
-                Matcher matcher = pattern.matcher(response.text());
-                if (matcher.find()) {
-                    int index = matcher.end();
-//                    System.out.println(response.text().substring(index, index + 100));
-                    String csrf = response.text().substring(index, index + 100);
-                }
-            }
-        }
+        login();
+    }
 
+    public void login() {
+        // Login
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setSlowMo(100).setHeadless(false));
+            Page page = browser.newPage();
+
+            Response response = page.navigate("https://connect.garmin.com/signin/");
+            page.waitForTimeout(2000);
+            FrameLocator signinFrame = page.frameLocator("#gauth-widget-frame-gauth-widget");
+            signinFrame.locator("#username").fill(this.Username);
+            page.waitForTimeout(1234);
+            signinFrame.locator("#password").fill(this.Password);
+            page.waitForTimeout(100);
+            signinFrame.locator("#login-btn-signin").click();
+            page.waitForTimeout(102325);
+
+
+        }
     }
 
     public boolean GarminLogin() {
