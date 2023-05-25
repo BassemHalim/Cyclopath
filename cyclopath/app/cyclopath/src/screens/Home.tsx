c337b9f9be76a3d9d17d69fa9a3ee3a1c64f7e91
BassemHalim <bassemamir459@gmail.com>
2023-05-24 18:48:51 -0700

    removed unused code

--- cyclopath/app/cyclopath/src/screens/Home.tsx
+++ cyclopath/app/cyclopath/src/screens/Home.tsx
@@ -61,32 +61,3 @@ export default function Home() {
   );
 }
 
-interface ActivityDTO {
-  activityId: number;
-  activityName: string;
-  averageHR: number;
-  averageSpeed: number;
-  bmrCalories: number;
-  calories: number;
-  distance: number;
-  duration: number;
-  elapsedDuration: number;
-  elevationCorrected: boolean;
-  elevationGain: number;
-  elevationLoss: number;
-  endLatitude: number;
-  endLongitude: number;
-  locationName: string;
-  maxElevation: number;
-  maxHR: number;
-  maxSpeed: number;
-  maxVerticalSpeed: number;
-  minActivityLapDuration: number;
-  minElevation: number;
-  movingDuration: number;
-  startLatitude: number;
-  startLongitude: number;
-  startTimeGMT: string;
-  startTimeLocal: string;
-  timeZoneId: number;
-}
